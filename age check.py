cb6499b6717f70577041d9d820132b689b2b78b9
Vihaan-Taparia <mayataparia@gmail.com>
2025-05-10 22:06:11 +0530

    Add files via upload

---
+++ age check.py
@@ -0,0 +1,5 @@
+age=int(input("Enter your age...."))
+if age>10 and age<20:
+    print("Your age is valid.....you are allowed inside the mall")
+else:
+    print("Your age is not valid.....you may not go in")
